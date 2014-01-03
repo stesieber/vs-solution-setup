05285992f344331a2dbb5e8aca94209828bfbc71
sie <stefan.sieber@online.ch>
2014-01-03 10:35:24 +0100

    Added file inside folder - pattern still works.

---
+++ SolutionExperiments/Src/Package1/ExampleClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionExperiments.Src.Package1
+{
+	/// <summary>
+	/// Example of a class that remains inside a package.
+	/// </summary>
+	class ExampleClass
+	{
+	}
+}
